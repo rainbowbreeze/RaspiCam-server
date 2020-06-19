368c54112fede57d7f692720e890ae1524b3fa14
Rainbowbreeze <rainbowbreeze@gmx.com>
2020-06-19 17:48:38 +0200

    fix

--- scripts/capture_frame.sh
+++ scripts/capture_frame.sh
@@ -17,7 +17,7 @@ check_for_rainbow_notify_and_curl()  {
 main() {
     # call the RaspiCam Server with the url to save the current frame
     #curl http://127.0.0.1:8000/capture.html
-    curl --fail http://localhost:8000/capture.html
+    curl --fail http://localhost:8000/capture.html 2>/dev/null;
 
     local result_code=$?
     # As per curl exit statuses at https://ec.haxx.se/usingcurl/usingcurl-returns ,
@@ -25,7 +25,7 @@ main() {
     #  desired
     #  The  -f, --fail has to be used to obtain the status code. Otherwise
     #   it's always 0, regardless the server error
-    if [ ! "$result_code" = "0" ]; then
+    if [ "$result_code" = "0" ]; then
         echo "Error with result code: ${result_code}"
         # Some error happened
         rainbow-notfy-admin.sh "Cannot extract a frame from the camera, error code $result_code"
